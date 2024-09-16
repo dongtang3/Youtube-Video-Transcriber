798ba6df6d8a6fd45218efc06f69e0c478585728
Tanish Kandivlikar <tanish.kandivlikar1412@gmail.com>
2024-09-16 13:24:37 -0400

    hugging face sync

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 import os
+import re
 from youtube_transcript_api import YouTubeTranscriptApi
 from huggingface_hub import InferenceClient  # Hugging Face Inference Client
 
@@ -14,10 +15,24 @@ client = InferenceClient()
 # Prompt for summarization
 prompt = """You are a Youtube Video Summarizer. You will take the transcript text and summarize the entire video, providing the important summary in points within 250 words. Please provide the summary of the text given here: """
 
+# Helper function to extract video ID from various YouTube URL formats
+def extract_video_id(youtube_url):
+    # Match for different YouTube URL structures
+    youtube_regex = (r'(https?://)?(www\.)?'
+                     '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                     '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+    youtube_match = re.match(youtube_regex, youtube_url)
+    
+    if youtube_match:
+        return youtube_match.group(6)
+    else:
+        raise ValueError("Invalid YouTube URL")
+
 # Function to extract transcript from YouTube video
 def extract_transcript_details(youtube_video_url):
     try:
-        video_id = youtube_video_url.split("=")[1]
+        # Extract the video ID
+        video_id = extract_video_id(youtube_video_url)
         print(video_id)
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
         transcript = ""
@@ -41,7 +56,7 @@ st.title("YouTube Transcript to Detailed Notes Converter")
 youtube_link = st.text_input("Enter the YouTube video Link:")
 
 if youtube_link:
-    video_id = youtube_link.split("=")[1]
+    video_id = extract_video_id(youtube_link)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Detailed Notes"):
@@ -53,4 +68,3 @@ if st.button("Get Detailed Notes"):
         summary = generate_huggingface_summary(transcript_text, prompt)
         st.markdown("## Detailed Notes")
         st.write(summary)
-        
